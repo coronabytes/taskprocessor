3078c2b4084969596f7a99997718940541941bb7
jung <jung@corona-bytes.net>
2023-11-15 02:42:45 +0100

    net8.0

--- Core.TaskProcessor.Tests/CronosTests.cs
+++ Core.TaskProcessor.Tests/CronosTests.cs
@@ -1,21 +1,10 @@
 using Cronos;
-using Newtonsoft.Json;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Core.TaskProcessor.Tests;
 
 public class CronosTests
 {
-    private readonly ITestOutputHelper _output;
-    private readonly TaskProcessor _processor;
-
-    public CronosTests(ITestOutputHelper output)
-    {
-        _output = output;
-       
-    }
-
     [Fact]
     public void Parse()
     {
